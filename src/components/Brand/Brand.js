567e0ff9bbdcc4ff4eef224cd98555b2f92a284f
L23550334 <143562935+L23550334@users.noreply.github.com>
2023-11-15 18:46:53 -0600

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -5,13 +5,14 @@ import * as styles from './Brand.module.css';
 
 const Brand = (props) => {
   return (
+    <center>
     <div
       className={styles.root}
       role={'presentation'}
       onClick={() => navigate('/')}
     >
       {/* <h4>SNKRS</h4> */}
-      <h1>Mi Imagen</h1>
+     
 
       {/* https://play-lh.googleusercontent.com/kWXy8EJ9rL4iH2lDxiDv0LYd6DeTPHLbzGXDHwZLvRy5UBhYHezTDm51onReoWZdzjPX */}
       <img
@@ -19,6 +20,7 @@ const Brand = (props) => {
         alt="SNIKERS MX"
       />
     </div>
+          </center>
   );
 };
 
